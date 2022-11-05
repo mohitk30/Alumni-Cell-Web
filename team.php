50e7a5cba5245b5146a49157791177bccee26c9b
Deepak Sharma <deepsrboy&gmail.com>
2022-11-05 17:08:32 +0530

    modified team.php

--- team.php
+++ team.php
@@ -17,21 +17,15 @@
     <link rel="stylesheet" href="assets/css/date-picker.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/gallery.css">
+    <link rel="stylesheet" href="assets/css/pages-css/team.css">
     <link rel="stylesheet" href="assets/css/responsive.css">
     <link rel="icon" type="image/png" href="assets/images/favicon.png">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 
     <title>Team @Alumni Cell</title>
 </head>
 
 <body>
-
-    <!-- <div class="preloader">
-        <div class="lds-ripple">
-        <div class="pl-flip-1 pl-flip-2"></div>
-        </div>
-        </div> 
-    -->
-
     <header class="header-area">
 
         <?php
@@ -69,27 +63,123 @@
         </div>
     </div>
 
-
-    <body>
-
-        <div class="p-5">
-
-            <!-- Team -->
-            <div class="team-card" style="width:300px;height:500px;padding:5px;">
-                <img src="assets/images/team/yash.jpg" alt="team" class="team-img" height="400px" width="300px">
-                <div class="team-card-detail" style="height:100px;">
-                    <h1 style="text-align: center;">Yash Phogat</h1>
-                    <p style="font-size: 1rem;text-align: center;">President</p>
-                    <p style="font-size: 1rem;text-align: center;">Contact No:-9667650530</p>
-
+    <div class="team-container">
+        <div class="row mt-2 text-center">
+            <div class="col-md-10 mx-auto team-inner-container text-center my-3 shadow">
+                <div class="row">
+                    <div class="col-12 my-2">
+                        <h1>The TEAM 2022</h1>
+                        <hr>
+                    </div>
+                </div>
+                <div class="row d-flex justify-content-evenly team-member-block">
+                    <!-- HERE TEAM CARD WILL BE DISPLAYED  -->
+                    <!--    DEMOCARD
+                    <div class="col-md-3 col-11 my-4 mx-md-3">
+                        <div class="card">
+                            <img class="card-img-top" src="assets/images/team/yash.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <p class="card-text text-center">
+                                <div id="member-name">Deepak Sharma</div>
+                                <div id="member-occupation">(Web Head)</div>
+                                </p>
+                                <div class="social-link-container">
+                                    <a href="#" target="_blank"><i class="bi bi-linkedin"></i></a>
+                                    <a href="tel:+91-9992912345" target="_blank"><i class="bi bi-telephone-fill"></i></a>
+                                    <a href="mailto:123@gmail.com" target="_blank"><i class="bi bi-envelope-fill"></i></a>
+                                    <a href="#" target="_blank"><i class="bi bi-instagram"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div> -->
                 </div>
             </div>
-
-
-
-
         </div>
-    </body>
+    </div>
 
+    <!-- <div class="p-5">
+        <div class="team-card" style="width:300px;height:500px;padding:5px;">
+            <img src="assets/images/team/yash.jpg" alt="team" class="team-img" height="400px" width="300px">
+            <div class="team-card-detail" style="height:100px;">
+                <h1 style="text-align: center;">Yash Phogat</h1>
+                <p style="font-size: 1rem;text-align: center;">President</p>
+                <p style="font-size: 1rem;text-align: center;">Contact No:-9667650530</p>
+            </div>
+        </div>
+    </div> -->
+
+</body>
+
+<script>
+    const TeamArr = [{
+            name: 'Deepak Sharma',
+            occupation: 'Web Head',
+            imglink: 'assets/images/team/yash.jpg',
+            linkdin: '#',
+            phone: '+91-9992912345',
+            email: '123@gmail.com',
+            instagram: '#'
+        },
+        {
+            name: 'Deepak Sharma',
+            occupation: 'Web Head',
+            imglink: 'assets/images/team/yash.jpg',
+            linkdin: '#',
+            phone: '+91-9992912345',
+            email: '123@gmail.com',
+            instagram: '#'
+        },
+        {
+            name: 'Deepak Sharma',
+            occupation: 'Web Head',
+            imglink: 'assets/images/team/yash.jpg',
+            linkdin: '#',
+            phone: '+91-9992912345',
+            email: '123@gmail.com',
+            instagram: '#'
+        },
+        {
+            name: 'Deepak Sharma',
+            occupation: 'Web Head',
+            imglink: 'assets/images/team/yash.jpg',
+            linkdin: '#',
+            phone: '+91-9992912345',
+            email: '123@gmail.com',
+            instagram: '#'
+        },
+        {
+            name: 'Deepak Sharma',
+            occupation: 'Web Head',
+            imglink: 'assets/images/team/yash.jpg',
+            linkdin: '#',
+            phone: '+91-9992912345',
+            email: '123@gmail.com',
+            instagram: '#'
+        }
+    ]
+
+    const TeamContainer = document.querySelector('.team-member-block');
+
+    TeamArr.map((obj) => {
+        const litral = `<div class="col-md-3 col-11 my-4 mx-md-3">
+                        <div class="card">
+                            <img class="card-img-top" src='${obj.imglink}' alt="Card image cap">
+                            <div class="card-body">
+                                <p class="card-text text-center">
+                                <div id="member-name">${obj.name}</div>
+                                <div id="member-occupation">(${obj.occupation})</div>
+                                </p>
+                                <div class="social-link-container">
+                                    <a href='${obj.linkdin}' target="_blank"><i class="bi bi-linkedin"></i></a>
+                                    <a href="tel:${obj.phone}" target="_blank"><i class="bi bi-telephone-fill"></i></a>
+                                    <a href="mailto:${obj.email}ank"><i class="bi bi-envelope-fill"></i></a>
+                                    <a href="${obj.instagram}" target="_blank"><i class="bi bi-instagram"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`
+        TeamContainer.innerHTML += litral;
+    });
+</script>
 
 </html>
